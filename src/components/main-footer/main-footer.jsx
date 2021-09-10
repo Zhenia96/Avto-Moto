e4b7eca1b3c4ba506d8931140a9080235debb215
Yauheni <kirichenkozhenia@mail.ru>
2021-09-11 00:23:36 +0300

    create main-footer

--- src/components/main-footer/main-footer.jsx
+++ src/components/main-footer/main-footer.jsx
@@ -2,9 +2,34 @@ import React from 'react';
 import './main-footer.scss';
 
 function MainFooter() {
-  return (
-    <footer>
 
+  return (
+    <footer className='main-footer'>
+      <div className='wrapper'>
+        <ul className='main-footer__menu'>
+          <li className='main-footer__menu-item'>
+            <a className='main-footer__menu-link' href='/'>Корпоративным клиентам</a>
+          </li>
+          <li className='main-footer__menu-item'>
+            <a className='main-footer__menu-link' href='/'>Клиентам</a>
+          </li>
+          <li className='main-footer__menu-item'>
+            <a className='main-footer__menu-link' href='/'>Аренда авто</a>
+          </li>
+          <li className='main-footer__menu-item'>
+            <a className='main-footer__menu-link' href='/'>Каршеринг</a>
+          </li>
+          <li className='main-footer__menu-item'>
+            <a className='main-footer__menu-link' href='/'>Как продать авто</a>
+          </li>
+          <li className='main-footer__menu-item'>
+            <a className='main-footer__menu-link' href='/' lang='en'>Trade-in</a>
+          </li>
+          <li className='main-footer__menu-item'>
+            <a className='main-footer__menu-link' href='/' lang='en'>Test drive</a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 }
